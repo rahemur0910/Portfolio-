fe3d95693d186c5c9f4a057f47741d8fc438c6da
rahemur0910 <rahemur1111rup.katha@gmail.com>
2025-04-30 12:37:44 +0530

    19

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -53,9 +53,9 @@ function ContactForm() {
     <div>
       <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">Contact with me</p>
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
-        <p className="text-sm text-[#d3d8e8]">
-          If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests.
-        </p>
+      <p className="text-sm text-[#d3d8e8]">
+        {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+      </p>
         <div className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
             <label className="text-base">Your Name:</label>
